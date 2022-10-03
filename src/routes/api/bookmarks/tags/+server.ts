6a67cf08ec217757bf2219a86a16c765ef33d8ea
aburto22 <aburto22@gmail.com>
2022-10-03 16:27:13 +0200

    removed unused file

--- src/routes/api/bookmarks/tags/+server.ts
+++
@@ -1,8 +0,0 @@
-import { getTags } from '$lib/server/links';
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	const tags = await getTags();
-	return json(tags);
-};
