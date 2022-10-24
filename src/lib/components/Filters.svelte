9c6f302e20ae7db8e860263de291c3a21d6da205
aburto22 <aburto22@gmail.com>
2022-10-24 16:14:28 +0200

    added collapsable filters on mobile

--- src/lib/components/Filters.svelte
+++ src/lib/components/Filters.svelte
@@ -1,21 +1,58 @@
 <script lang="ts">
 	import { getLink, getName } from '$lib/utils/links';
 	import tags from '$lib/stores/tags';
+	import Svg from '$lib/components/Svg.svelte';
+	import { onMount } from 'svelte';
 
 	export let currentTag = '';
+	let areFiltersOpen = false;
+
+	let height: number = 0;
+	let hideChevron = false;
+	let list: HTMLElement | null = null;
+
+	const checkHeight = () => {
+		height = list?.scrollHeight || 0;
+		hideChevron = height < 40;
+	};
+
+	onMount(() => {
+		checkHeight();
+	});
+
+	$: heightStyle = `--height: ${height}px;`;
+
+	const handleClick = () => {
+		areFiltersOpen = !areFiltersOpen;
+	};
 </script>
 
-<ul>
-	{#each $tags as tag}
-		<li>
-			<a class:current={currentTag === tag} href={currentTag === tag ? '/' : `${getLink(tag)}`}>
-				{getName(tag)}
-			</a>
-		</li>
-	{/each}
-</ul>
+<svelte:window on:resize={checkHeight} />
 
-<style>
+<div>
+	<ul id="filter-container" class:expanded={areFiltersOpen} style={heightStyle} bind:this={list}>
+		{#each $tags as tag}
+			<li>
+				<a class:current={currentTag === tag} href={currentTag === tag ? '/' : `${getLink(tag)}`}>
+					{getName(tag)}
+				</a>
+			</li>
+		{/each}
+	</ul>
+	<button on:click={handleClick} class:hide={hideChevron}>
+		{#if areFiltersOpen}
+			<Svg name="chevron-up" width="1.2rem" height="1.2rem" />
+		{:else}
+			<Svg name="chevron-down" width="1.2rem" height="1.2rem" />
+		{/if}
+	</button>
+</div>
+
+<style lang="scss">
+	div {
+		display: flex;
+		align-items: flex-start;
+	}
 	ul {
 		display: flex;
 		flex-wrap: wrap;
@@ -23,7 +60,19 @@
 		gap: 0.5rem;
 		width: 100%;
 		max-width: 35rem;
-		margin: 0 auto 2rem;
+		margin: 0 auto 1rem;
+		max-height: 2.5rem;
+		overflow: hidden;
+		transition: max-height 200ms;
+
+		@media (min-width: 50rem) {
+			max-height: unset;
+			margin-bottom: 2rem;
+		}
+	}
+
+	.expanded {
+		max-height: var(--height);
 	}
 
 	li {
@@ -51,5 +100,17 @@
 	.current:hover {
 		background-color: var(--yellow);
 		color: var(--darkBlue);
+	}
+
+	button {
+		padding: 0.6rem;
+
+		@media (min-width: 50rem) {
+			display: none;
+		}
+	}
+
+	.hide {
+		display: none;
 	}
 </style>
